5eba6a8cb10aaeb1b6673d4ea89d11651bcab6ce
Hosen Md Maruf (DC/SIC) <hom2ul@bosch.com>
2024-09-07 12:40:24 +0200

    nav updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
